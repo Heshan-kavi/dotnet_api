02be525bb175a4a0c02c3a29eac3140cc1644e3c
Hesh-Kavinda <heshan@ascnetic.se>
2023-10-03 11:59:18 +0530

    implemented new route in the character controller

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -10,11 +10,23 @@ namespace dotnet_api.Controllers
     [Route("api/[controller]")]
     public class CharacterController : ControllerBase
     {
-        private static Character knight = new Character();
+        private static List<Character> characters = new List<Character>{
+            new Character(),
+            new Character{
+                Name = "Sam"
+            }
+        };
+
+        [HttpGet("GetAll")]
+        public ActionResult<List<Character>> Get(){
+            return Ok(characters);
+        }
 
         [HttpGet]
-        public ActionResult<Character> Get(){
-            return Ok(knight);
+        public ActionResult<Character> GetSingleCharacter(){
+            return Ok(characters[0]);
         }
+
+        
     }
 }
