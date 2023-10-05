4a58e362bcee5b401bf5532c8a48867dcaf56a19
Hesh-Kavinda <heshan@ascnetic.se>
2023-10-05 09:31:16 +0530

    implemented connection string successfully

--- Program.cs
+++ Program.cs
@@ -1,11 +1,15 @@
 global using dotnet_api.Models ;
 global using dotnet_api.Services.CharacterService ;
 global using dotnet_api.Dtos.Character ;
+global using Microsoft.EntityFrameworkCore;
+global using dotnet_api.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
