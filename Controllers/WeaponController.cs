15e0d1122da0981a483ff593d8ed31e0938267da
Hesh-Kavinda <heshan@ascnetic.se>
2023-10-12 16:38:58 +0530

    implemented weapon controller realted things

---
+++ Controllers/WeaponController.cs
@@ -0,0 +1,30 @@
+using System.Net;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Authorization;
+
+namespace dotnet_api.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class WeaponController : ControllerBase
+    {
+        private readonly IWeaponService _weaponService;
+
+        public WeaponController(IWeaponService weaponService)
+        {
+            _weaponService = weaponService;
+        }
+
+       [HttpPost]
+       public async Task<ActionResult<ServiceResponse<GetCharacterDto>>> AddWeapon(AddWeaponDto newWeapon){
+            return Ok(await _weaponService.AddWeapon(newWeapon));
+       }
+    }
+}
