30259911f13146e7f166dbc028d3b452a92bf033
Hesh-Kavinda <heshan@ascnetic.se>
2023-10-13 19:37:08 +0530

    created the interface and the serivce

---
+++ Services/Fight/FightService.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnet_api.Services.Fight
+{
+    public class FightService : IFightService
+    {
+        public async Task<ServiceResponse<GetCharacterDto>> GetFights(){
+            
+        }
+    }
+}
